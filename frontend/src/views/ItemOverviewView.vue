9d9dc4baeaf2d90817e2050616eaf4817b7b6c39
Joep Buhre <jcbuhre@gmail.com>
2023-04-07 22:43:23 +0200

    Wip

--- frontend/src/views/ItemOverviewView.vue
+++ frontend/src/views/ItemOverviewView.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        <div @click="editItem = item" class="my-5 border solid border-gray-300 py-1 px-2" v-for="item in items">
+        <InputGroup
+            class="my-0"
+            v-model="search"
+            name="Search"
+            prettyname="search"
+            :compact="true"
+        />
+        <div @click="editItem = item" class="my-5 border solid border-gray-300 py-1 px-2" v-for="item in visibleItems">
             <strong>{{ item.ProductName }}</strong>    
             <p>{{ item.EAN }}</p>    
         </div>
@@ -28,7 +35,7 @@
 <script setup lang="ts">
 import type { Items } from '.prisma/client';
 import { api } from '../utils/api';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import ThePopup from '../components/ThePopup.vue';
 import { raw } from '../utils/helpers';
 import ItemEditModal from '../components/ItemEditModal.vue';
@@ -36,6 +43,45 @@ import InputGroup from '../components/InputGroup.vue';
 
 const items = ref<Items[]>([])
 const editItem = ref<Items | null>(null)
+const search = ref<string>('')
+
+const visibleItems = computed(() => {
+    return items.value
+        ?.filter((item: Items) => {
+            return (
+                item.EAN.search(search.value) > -1 ||
+                (item.ProductName || "")
+                    ?.toLowerCase()
+                    .search(search.value.toLowerCase()) > -1
+            );
+        })
+        // .map((it) => {
+        //     return {
+        //         ...it,
+        //         Stock: it.Inventory.reduce((a, b) => a + b.Stock, 0),
+        //         Expiry: new Date(
+        //             Math.min.apply(
+        //                 0,
+        //                 it.Inventory.map((el) => new Date(el.Expiry).getTime())
+        //             )
+        //         ),
+        //         Location: it.Inventory.map((el) => el?.Location?.Name).join(
+        //             ", "
+        //         ),
+        //     };
+        // })
+        .sort((a, b) => {
+            if (a.ProductName && b.ProductName) {
+                if (a.ProductName < b.ProductName) {
+                    return -1;
+                }
+                if (a.ProductName > b.ProductName) {
+                    return 1;
+                }
+            }
+            return 0;
+        });
+});
 
 
 const getItems = () => {
